fdca836577093b6fdbadea744f781deb45010f50
iradufasha <bikrimwiza@gmail.com>
2022-10-14 12:34:52 -0700

    second project to get the last gigit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,49 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <time.h>
+
+/* more headers goes there */
+
+
+
+/* betty style doc for function main goes there */
+/**
+ * main - just main
+ * Description : this main is on roll
+*/
+int main(void)
+
+{
+
+	int n;
+
+
+
+	srand(time(0));
+
+	n = rand() - RAND_MAX / 2;
+
+	/* your code goes there */
+	printf("Last digit of %d", n);
+	mod = n%10;
+	printf("is %d", mod);
+	if (mod == 5)
+	{
+		printf(" and is greater than 5\n");
+	}
+	else if (mod == 0)
+	{
+		printf(" and is 0\n");
+	}
+	else if (mod < 6 && mod != 0)
+	{
+		printf(" and is less than 6 and not 0\n");
+	}
+	else
+	{
+		printf("sory unknown command\n");
+	}
+	return (0);
+
+}
