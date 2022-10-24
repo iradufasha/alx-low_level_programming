64ab04c97b82aa53306de9b0dc7be98ad81fc3e8
iradufasha <bikrimwiza@gmail.com>
2022-10-24 10:42:47 -0700

    a function that swaps the values of two integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+/**
+ * swap_int - my swiper
+ * @a: value 1
+ * @b: value 2
+ * return: 0
+ **/
+void swap_int(int *a, int *b)
+
+{
+
+	int temp = *a;
+	int tempb = *b;
+
+	*a = tempb;
+	*b = temp;
+
+	return (0);
+
+}
