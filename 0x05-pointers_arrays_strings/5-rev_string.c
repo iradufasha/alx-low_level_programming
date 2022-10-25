03b041c19c3ba0a01c06251837b29c52a6453c02
iradufasha <bikrimwiza@gmail.com>
2022-10-25 08:24:56 -0700

    let go

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+
+
+/**
+* rev_string - simple
+* @s: argument1
+* Descripton: go
+* Return: 0
+**/
+
+
+
+void rev_string(char s[]) /* interpreted as void rev_string(char *s) */
+
+{
+
+	int i = 0, j = 0;
+
+	char str[500];
+
+	while (*(s + i) != '\0')
+
+	{
+
+		*(str + i) = *(s + i);
+
+		i++;
+
+	}
+
+	int y = i - 1;
+
+	while (y >= 0)
+
+	{
+
+		*(s + y) = *(str + j);
+
+		j++;
+
+		y--;
+
+	}
+
+}
