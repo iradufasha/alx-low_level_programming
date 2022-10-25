a0b288d073af9255ff73a69ccc9750e7b3bda4ce
iradufasha <bikrimwiza@gmail.com>
2022-10-24 19:23:10 -0700

    go

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -8,12 +8,12 @@
 
 void _puts(char *str)
 {
-	int i = 0;
+	int i =0;
 
-	while (*(str + i))
+	while (str[i])
 	{
-		_putchar(*(str + i));
+		_putchar(str[i]);
 		i++;
 	}
-	_putchar('\n');
+	_putchar('\n);
 }
