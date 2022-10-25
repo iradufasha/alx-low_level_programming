f2522d06da2eead33923bb12927a7404f9bcd8c2
iradufasha <bikrimwiza@gmail.com>
2022-10-25 08:40:33 -0700

    go

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -15,7 +15,7 @@ void rev_string(char s[]) /* interpreted as void rev_string(char *s) */
 
 {
 
-	int i = 0, j = 0;
+	int y, int i = 0, j = 0;
 
 	char str[500];
 
@@ -29,7 +29,7 @@ void rev_string(char s[]) /* interpreted as void rev_string(char *s) */
 
 	}
 
-	int y = i - 1;
+	y = i - 1;
 
 	while (y >= 0)
 
