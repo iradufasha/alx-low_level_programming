c5a9dfd94fdf2c8762ff44452f8781e1ec08faed
iradufasha <bikrimwiza@gmail.com>
2022-10-25 19:31:07 -0700

    function that prints every other character of a string, starting with the first character, followed by a new line.

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+
+
+/**
+* _putchar - simple function to print divisible in a string
+* @c: argument1
+* Description: holberton printer
+* Return: 0
+**/
+
+
+
+int _putchar(char c);
+
+
+
+/**
+* puts2 - simple function to print divisible in a string
+* @str: argument1
+* Description: go
+* Return: 0
+**/
+
+
+
+void puts2(char *str) /* also same as void puts2(char str[]) */
+
+{
+
+	int i = 0;
+
+
+
+	while (*(str + i))
+
+	{
+
+		if (*(str + i) % 2 == 0)
+
+		{
+
+			_putchar("%c", *(str + i));
+
+		}
+
+
+
+		i++;
+
+	}
+
+}
