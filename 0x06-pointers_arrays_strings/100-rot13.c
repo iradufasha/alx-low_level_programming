9852d5ca4eac1f4105a0c1a60e547d04539c6bc5
iradufasha <bikrimwiza@gmail.com>
2022-10-28 20:52:36 -0700

    haso

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+
+
+
+
+/**
+* rot13 - my strncat
+* @s: string to be converted
+* Description: converts string using rot13
+* Return: s
+**/
+
+
+
+char *rot13(char *s)
+{
+
+	int i, j;
+
+	j = 0;
+
+	char oner[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+		'M', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+				 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+				 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+	char laster[] = {'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
+		'Z', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+				 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+				 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'};
+
+
+
+	while (*(s + j) != '\0')
+	{
+
+		for (i = 0; i < 52; i++)
+		{
+			if (*(s + j) == *(oner + i))
+
+			{
+				*(s + j) = *(laster + i);
+				break;
+			}
+		}
+
+		j++;
+	}
+
+	return (s);
+
+}
